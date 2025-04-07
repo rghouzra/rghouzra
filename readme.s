54ea76a63af8f68e54f7e21ed50e7a328643fbff
reda ghouzraf <98324229+rghouzra@users.noreply.github.com>
2025-04-07 10:42:09 +0100

    Create readme.s

---
+++ readme.s
@@ -0,0 +1,39 @@
+section .data
+    profile_info db "Hi, I'm Reda Ghouzraf", 10
+    level_info db "A passionate low level developer", 10
+    languages db "Languages and Tools:", 10
+    languages_list db "C, Assembly, Bash", 10
+
+section .text
+    global _start
+
+_start:
+    mov eax, 4
+    mov ebx, 1
+    mov ecx, profile_info
+    mov edx, 22
+    int 0x80
+    mov eax, 4
+    mov ebx, 1
+    mov ecx, level_info
+    mov edx, 33
+    int 0x80
+
+    
+    mov eax, 4
+    mov ebx, 1
+    mov ecx, languages
+    mov edx, 21
+    int 0x80
+
+    mov eax, 4
+    mov ebx, 1
+    mov ecx, languages_list
+    mov edx, 19
+    int 0x80
+
+    mov eax, 1          ; Syscall sys_exit --
+    xor ebx, ebx        ; for 0 return
+    int 0x80
+
+; you can see the output of this code in https://godbolt.org/z/njn1MPc7o
